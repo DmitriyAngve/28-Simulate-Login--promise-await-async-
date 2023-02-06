f6be0bf839e2c684dd6bad71acfd7174aa78c021
Dmitriy Angve <kirirch129@gmail.com>
2023-02-06 13:15:25 +0300

    #

--- app.js
+++ app.js
@@ -11,6 +11,18 @@ const userName = adder(output, "input", false, "text", false);
 const userPsw = adder(output, "input", false, "password", false);
 const btn = adder(output, "button", "Login", false, "btn");
 
+btn.addEventListener("click", checker);
+
+function checker(e) {
+  if (userName.value.length > 3 && userPsw.value.length > 3) {
+    console.log(userName.value, userPsw.value);
+    addStyler([userName, userPsw], "");
+  } else {
+    addStyler([userName, userPsw], "red");
+  }
+  console.log(userName.value, userPsw.value);
+}
+
 function adder(parent, eleType, html, att, cla) {
   const ele = document.createElement(eleType);
   if (html) ele.innerHTML = html;
@@ -18,4 +30,11 @@ function adder(parent, eleType, html, att, cla) {
   if (cla) ele.classList.add(cla);
   //   return parent.append(ele);
   return parent.appendChild(ele); // ap"endChild()" возвращает элемент в отличие от "append()"
+}
+
+function addStyler(eles, colr) {
+  eles.forEach((ele) => {
+    ele.style.color = colr;
+    ele.style.borderColor = colr;
+  });
 }
